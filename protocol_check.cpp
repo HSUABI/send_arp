736ecdbed9a0e44fc992bea2a575dba68c193a26
gullabjamun <supermariocake@google.com>
2018-07-28 21:12:55 +0900

    Add arp check

--- protocol_check.cpp
+++ protocol_check.cpp
@@ -1,10 +1,16 @@
 #include "protocol_check.h"
 #define IPPROTO_TCP 0X06
 #define ETHERTYPE_IP 0X0800
+#define ETHERTYPE_ARP 0X0806
 
 int ip_check(unsigned short type)
 {
 	return type==ETHERTYPE_IP ? 1: 0 ;
+}
+
+int arp_check(unsigned short type)
+{
+	return type==ETHERTYPE_ARP ? 1: 0 ;
 }
 
 int tcp_check(unsigned short protocol)
